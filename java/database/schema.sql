d61ab94c6baa083f3f3bf06fbeac54381afb98e6
Christopher Spencer <cdspenzer@gmail.com>
2023-08-06 14:58:38 -0400

    added sql table for landmarks

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,7 @@
 BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS landmarks;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -8,6 +9,19 @@ CREATE TABLE users (
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+
+CREATE TABLE landmarks (
+    landmark_id SERIAL,
+	name varchar(50) NOT NULL,
+	address varchar(200) NOT NULL,
+	latitude_coordinates decimal(8,6) NOT NULL,
+	longitude_coordinates decimal(9,6) NOT NULL,
+	image_name varchar(255) NOT NULL,
+	description varchar(200) NOT NULL,
+	historic_details varchar(200) NOT NULL,
+	cost_of_entry decimal(2,2),
+	CONSTRAINT PK_landmark PRIMARY KEY (landmark_id)
 );
 
 COMMIT TRANSACTION;
