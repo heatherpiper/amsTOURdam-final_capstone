8ae5f49fc8df18b3640be3dcafffdc09c8c8d651
charlene mitchell <lcmitchell23@hotmail.com>
2023-08-06 15:01:14 -0400

    pull for sql tables

---
+++ java/src/main/java/com/techelevator/dao/JdbcLandmarkDao.java
@@ -0,0 +1,20 @@
+package com.techelevator.dao;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+
+public class JdbcLandmarkDao implements LandmarkDao{
+
+    private JdbcTemplate jdbcTemplate;
+
+    public JdbcLandmarkDao(DataSource datasource) {
+        jdbcTemplate = new JdbcTemplate(datasource);
+
+    }
+
+
+    
+
+}
