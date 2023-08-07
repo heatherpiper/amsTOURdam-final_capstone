ef2db072e798c8cb60729dd93c270246c7ed4475
Christopher Spencer <cdspenzer@gmail.com>
2023-08-07 17:18:34 -0400

    push

--- vue/src/views/Landmarks.vue
+++ vue/src/views/Landmarks.vue
@@ -1,50 +1,26 @@
 <template>
   <div class="landmarks">
-<<<<<<< HEAD
-      <h1>Landmarks</h1>
-    <add-landmarks-comp />
-    <landmarks-comp />
-=======
     <LandmarksComp />
     <h1>Landmarks</h1>
->>>>>>> bd4f7a07cca7c50b1d17665897fd586af88bd033
+
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
 
-import AddLandmarksComp from '../components/AddLandmarksComp.vue'
 import LandmarksComp from '../components/LandmarksComp.vue'
 
 export default {
     components: {
         LandmarksComp,
-        AddLandmarksComp
-       
-       
-=======
-import LandmarksComp from '@/components/LandmarksComp'
-
-
-export default {
-    components: {
-        LandmarksComp
-        
->>>>>>> bd4f7a07cca7c50b1d17665897fd586af88bd033
     }
 }
 </script>
 
-<<<<<<< HEAD
-
-        LandmarksComp<style>
-
-=======
 <style>
 h1 {
   text-align: center;
   font-family: arial Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
 }
->>>>>>> bd4f7a07cca7c50b1d17665897fd586af88bd033
+
 </style>
