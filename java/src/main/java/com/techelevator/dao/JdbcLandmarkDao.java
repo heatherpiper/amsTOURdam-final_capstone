5fe6e0ac3a39b253420adc3b008c363dc53bdaf2
Christopher Spencer <cdspenzer@gmail.com>
2023-08-06 22:47:08 -0400

    updated backend controller and database to fix postman

--- java/src/main/java/com/techelevator/dao/JdbcLandmarkDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcLandmarkDao.java
@@ -19,15 +19,13 @@ public class JdbcLandmarkDao implements LandmarkDao{
     private JdbcTemplate jdbcTemplate;
 
     public JdbcLandmarkDao(DataSource final_capstone) {
-        jdbcTemplate = new JdbcTemplate(final_capstone);
+        this.jdbcTemplate = new JdbcTemplate(final_capstone);
     }
-
-
 
 public Landmark getLandmarkById(int landmarkId) {
 
         Landmark landmark = null;
-        String sql =    "SELECT name, address, latitude_coordinates, longitude_coordinates, " +
+        String sql =    "SELECT landmark_id, name, address, latitude_coordinates, longitude_coordinates, " +
                         "image_name, description, historic_details, cost_of_entry, reviews " +
                         "FROM landmarks WHERE landmark_id =?";
         try{
@@ -54,9 +52,9 @@ public Landmark getLandmarkById(int landmarkId) {
         Landmark landmark = new Landmark();
         landmark.setLandmark_id(rs.getInt("landmark_id"));
         landmark.setName(rs.getString("name"));
-        landmark.setAddress(landmark.getAddress());
-        landmark.setLatitude(rs.getDouble("latitude_coordinates"));
-        landmark.setLongitude(rs.getDouble("longitude_coordinates"));
+        landmark.setAddress(rs.getString("address"));
+        landmark.setLatitudeCoordinates(rs.getDouble("latitude_coordinates"));
+        landmark.setLongitudeCoordinates(rs.getDouble("longitude_coordinates"));
         landmark.setImageName(rs.getString("image_name"));
         landmark.setDescription(rs.getString("description"));
         landmark.setHistoricDetails(rs.getString("historic_details"));
