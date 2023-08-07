9f49f0c2efdf27898494e886144fdbf57a014c35
Christopher Spencer <cdspenzer@gmail.com>
2023-08-07 14:43:19 -0400

    vondelpark to front

--- vue/src/components/LandmarksComp.vue
+++ vue/src/components/LandmarksComp.vue
@@ -1,14 +1,21 @@
 <template>
-  <div>
-
+  <div id="landmarks">
+    <h1>Landmarks</h1>
+    <div class="LandmarksList">
+      <landmark-detail v-for="landmark in landmarks" v-bind:key="landmark.landmarkId" v-bind:landmark="landmarks"/>
+    </div>
   </div>
 </template>
 
 <script>
 import LandmarksService from '../services/LandmarksService'
+import LandmarkDetail from './LandmarkDetailComp.vue'
 
 export default {
 name: "landmarks",
+components: {
+  LandmarkDetail
+},
 data() {
     return {
         landmarks: []
@@ -17,6 +24,7 @@ data() {
  created() {
         LandmarksService.getAllLandmarks().then( response => {
         this.landmarks = response.data;
+        console.log(this.landmarks);
         })
     }
 }
