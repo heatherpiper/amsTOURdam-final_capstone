bda69634dd64345c956ee9ebed12a30c2aa77b22
Christopher Spencer <cdspenzer@gmail.com>
2023-08-08 00:48:25 -0400

    bullshit map

--- vue/src/components/GoogleMapsComp.vue
+++ vue/src/components/GoogleMapsComp.vue
@@ -2,26 +2,20 @@
   <div>
     <div>
       <h2>Search and add a pin</h2>
-      <GmapAutocomplete
-        @place_changed='setPlace'
-      />
-      <button
-        @click='addMarker'
-      >
-        Add
-      </button>
+      <GmapAutocomplete @place_changed="setPlace" />
+      <button @click="addMarker">Add</button>
     </div>
-    <br>
-    <GmapMap
-      :center='center'
-      :zoom='12'
-      style='width:100%;  height: 400px;'
-    >
+    <div>
+      <button @click="searchNearbyPlaces">Search Nearby Locations</button>
+    </div>
+    <br />
+    <GmapMap :center="center" :zoom="12" style="width: 400px; height: 400px">
       <GmapMarker
+        ref="map"
         :key="index"
         v-for="(m, index) in markers"
         :position="m.position"
-        @click="center=m.position"
+        @click="center = m.position"
       />
     </GmapMap>
   </div>
@@ -29,17 +23,26 @@
 
 <script>
 export default {
-  name: 'GoogleMap',
+  name: "GoogleMap",
   data() {
     return {
-      center: { lat: 45.508, lng: -73.587 },
+      center: { lat: 52.377956, lng: 4.89707 },
       currentPlace: null,
       markers: [],
       places: [],
-    }
+      service: null,
+    };
   },
   mounted() {
     this.geolocate();
+
+    if (this.$refs.map) {
+      this.service = new window.google.maps.places.PlacesService(
+        this.$refs.map.$mapObject
+      );
+    }
+
+    this.handleNearbySearchResults = this.handleNearbySearchResults.bind(this);
   },
   methods: {
     setPlace(place) {
@@ -57,18 +60,50 @@ export default {
         this.currentPlace = null;
       }
     },
-    geolocate: function() {
-      navigator.geolocation.getCurrentPosition(position => {
+    geolocate: function () {
+      navigator.geolocation.getCurrentPosition((position) => {
         this.center = {
           lat: position.coords.latitude,
           lng: position.coords.longitude,
         };
       });
     },
+    searchNearbyPlaces() {
+      if (this.center && this.service) {
+        const request = {
+          location: this.center,
+          radius: 1000,
+          type: 'restaurant'
+        };
+
+        this.service.nearbySearch(request, this.handleNearbySearchResults);
+      }
+    },
+
+    handleNearbySearchResults(results, status) {
+  if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+    this.markers = [];
+
+    results.forEach((result) => {
+      const marker = {
+        lat: result.geometry.location.lat(),
+        lng: result.geometry.location.lng(),
+      };
+      this.markers.push({ position: marker });
+    });
+
+    if (results.length > 0) {
+      const firstPlace = results[0];
+      this.center = {
+        lat: firstPlace.geometry.location.lat(),
+        lng: firstPlace.geometry.location.lng(),
+      };
+    }
+  }
+},
   },
 };
 </script>
 
 <style>
-
 </style>
