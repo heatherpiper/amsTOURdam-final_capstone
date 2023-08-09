9e1224cf943c9ea9785cd4b2ac8c63c8253fb7f7
Christopher Spencer <cdspenzer@gmail.com>
2023-08-09 00:17:36 -0400

    map update and css styling

--- vue/src/components/LandmarksComp.vue
+++ vue/src/components/LandmarksComp.vue
@@ -1,30 +1,40 @@
 <template>
   <div id="landmarks">
-    
-    <div class="LandmarksList" v-for="landmark in landmarks" v-bind:key="landmark.landmark_id">
-      <router-link v-bind:to="{
-        name:'landmarkdetail', 
-        params:{ id: landmark.landmark_id } }">
-        <h1>{{ landmark.name }}</h1>
-      </router-link>
-        <h2>{{ landmark.description}}</h2>
-      <!-- <landmark-detail v-for="landmark in landmarks" v-bind:key="landmark.landmarkId" v-bind:landmark="landmarks"/> -->
+    <div class="LandmarksGrid">
+      <div class="LandmarksList" v-for="landmark in landmarks" :key="landmark.landmark_id">
+        <router-link :to="{ name: 'landmarkdetail', params: { id: landmark.landmark_id } }">
+          <h1>{{ landmark.name }}</h1>
+        </router-link>
+        <h2>{{ landmark.description }}</h2>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-// import LandmarkDetail from './LandmarkDetailComp.vue'
-
 export default {
 name: "landmarks",
-components: {
-  // LandmarkDetail
-},
 props: ['landmarks']
 }
 </script>
 
-<style>
+<style scoped>
+.LandmarksGrid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr)); 
+  gap: 20px; 
+}
 
+.LandmarksList {
+  border: 10px solid rgb(102, 3, 89);
+  border-style: double;
+  padding: 20px;
+  background-color: rgb(231, 163, 17);
+  color: rgb(58, 2, 58);
+  font-family: Georgia, 'Times New Roman', Times, serif;
+}
+
+h2 {
+  font-size: 19px;
+}
 </style>
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -2,7 +2,9 @@
   <div class="home">
     <h1>Welcome To Amsterdam!</h1>
     <p></p>
-    <GoogleMapsCompVue/>
+    <div class="googlemap-container">
+      <GoogleMapsCompVue/>
+    </div>
   </div>
 </template>
 
@@ -16,9 +18,27 @@ export default {
   }
 };
 </script>
+
 <style scoped>
 h1 {
   text-align: center;
-  font-family: arial Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+  font-size: 60px;
+  border: 10px solid rgb(102, 3, 89);
+  border-style: double;
+  color: rgb(58, 2, 58);
+  font-family: Georgia, 'Times New Roman', Times, serif;
+  background-color: rgb(231, 163, 17);
+  width: 900px;
+  margin: 0 auto;
+}
+
+div home {
+  position: relative;
+}
+
+div .googlemap-container {
+  position: absolute;
+  left: 48%;
+  
 }
 </style>
