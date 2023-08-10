1c5edfc3d217aec4ec78c8f2a0c00202a766fd07
heatherpiper <heather.a.piper@gmail.com>
2023-08-10 14:50:55 -0400

    formatted sql table for all landmarks

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -13,18 +13,19 @@ CREATE TABLE users (
 
 CREATE TABLE landmarks (
     landmark_id SERIAL,
+    category varchar(50),
 	name varchar(50) NOT NULL,
 	street varchar (100) NOT NULL,
 	house_number integer NOT NULL,
 	postal_code varchar (10) NOT NULL,
 	city varchar (50) NOT NULL,
-	town varchar (50) NOT NULL,
 	latitude_coordinates decimal NOT NULL,
 	longitude_coordinates decimal NOT NULL,
 	image_name varchar(255) NOT NULL,
 	description varchar(200) NOT NULL,
 	historic_details varchar(200) NOT NULL,
 	cost_of_entry decimal,
+	duration decimal,
 	reviews varchar(500),
 	CONSTRAINT PK_landmark PRIMARY KEY (landmark_id)
 );
