442cd65d7e6a0cf43ef755d17a9eddc640e51076
Christopher Spencer <cdspenzer@gmail.com>
2023-08-11 17:04:49 -0400

    working on backend, itinerary by user id

--- java/src/main/java/com/techelevator/controller/ItineraryController.java
+++ java/src/main/java/com/techelevator/controller/ItineraryController.java
@@ -2,20 +2,26 @@ package com.techelevator.controller;
 
 import com.techelevator.dao.ItineraryDao;
 import com.techelevator.dao.LandmarkDao;
+import com.techelevator.dao.UserDao;
 import com.techelevator.model.Itinerary;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @CrossOrigin
 @RestController
+@PreAuthorize("isAuthenticated()")
 public class ItineraryController {
 
     private ItineraryDao itineraryDao;
+    private UserDao userDao;
 
-    public ItineraryController(ItineraryDao itineraryDao) {
+    public ItineraryController(ItineraryDao itineraryDao, UserDao userDao) {
         this.itineraryDao = itineraryDao;
+        this.userDao = userDao;
     }
 
     @GetMapping("/myitinerary/")
@@ -27,6 +33,13 @@ public class ItineraryController {
     public Itinerary getMyItinerary(@PathVariable int itineraryId) {
         Itinerary itinerary = itineraryDao.getMyItinerary(itineraryId);
         return itinerary;
+    }
+
+    @GetMapping("/{userId}/myitinerary/")
+    public List<Itinerary>  getItinerariesByUserId(Principal principal) {
+        String username = principal.getName();
+        int userId = userDao.findIdByUsername(username);
+        return itineraryDao.getItinerariesByUserId(userId);
     }
 
     @ResponseStatus(HttpStatus.CREATED)
