0678b2a16ebebabdb91f9e932cc027a5d9ea79a9
Christopher Spencer <cdspenzer@gmail.com>
2023-08-10 20:20:23 -0400

    itineraries

---
+++ vue/src/components/MyItinerariesComp.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <h1>My Itineraries:</h1>
+  </div>
+</template>
+
+<script>
+import ItineraryService from "@/services/ItineraryService";
+
+export default {
+      data() {
+    return {
+      itineraries: {}
+    };
+  },
+    created() {
+    ItineraryService.getItineraries().then((response) => {
+      this.itineraries = response.data;
+      console.log(this.itineraries);
+    });
+  }
+}
+</script>
+
+<style>
+
+</style>
