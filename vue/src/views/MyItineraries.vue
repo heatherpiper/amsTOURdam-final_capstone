9784f4e43cf64a825dbb9a0a0ae8ea9c13344aaa
Christopher Spencer <cdspenzer@gmail.com>
2023-08-10 20:20:23 -0400

    itineraries

---
+++ vue/src/views/MyItineraries.vue
@@ -0,0 +1,17 @@
+<template>
+  <MyItinerariesComp />
+</template>
+
+<script>
+import MyItinerariesComp from '../components/MyItinerariesComp.vue'
+
+export default {
+    components: {
+        MyItinerariesComp
+    }
+}
+</script>
+
+<style>
+
+</style>
