d20f04e2f65112ab7d08cf5769a5bf70773e8819
Christopher Spencer <cdspenzer@gmail.com>
2023-08-12 00:33:58 -0400

    shit for brains

--- vue/src/components/MyItinerariesComp.vue
+++ vue/src/components/MyItinerariesComp.vue
@@ -1,20 +1,39 @@
 <template>
-  <div>
+  <div id="itineraries">
       <h1>My Itineraries:</h1>
+      <div class="itenerayList">
+        <MyItinerary v-for="itinerary in itineraries"
+        v-bind:key="itinerary.userId"
+        v-bind:itinerary="itinerary" />
+        {{itinerary.itineraryName}}
+      </div>
   </div>
 </template>
 
 <script>
 import ItineraryService from "@/services/ItineraryService";
+import MyItinerary from "@/components/MyItineraryComp.vue"
 
 export default {
-      data() {
-    return {
-      itineraries: {}
-    };
+  name: 'itinerary-list',
+  components: {
+    MyItinerary
   },
-    created() {
-    ItineraryService.getItineraries().then((response) => {
+  data() {
+    return {
+      userId: 0,
+      itineraries: []
+    }
+  },
+  //   created() {
+  //   ItineraryService.getItineraries().then((response) => {
+  //     this.itineraries = response.data;
+  //     console.log(this.itineraries);
+      
+  //   });
+  // },
+  created() {
+    ItineraryService.getItinerariesByUserId(this.userId).then((response) => {
       this.itineraries = response.data;
       console.log(this.itineraries);
     });
