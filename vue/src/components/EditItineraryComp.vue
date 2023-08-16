8f411c3b8f6ccfa3d84f7c56035a220a00fe6b1b
Christopher Spencer <cdspenzer@gmail.com>
2023-08-16 19:30:23 -0400

    css crap

--- vue/src/components/EditItineraryComp.vue
+++ vue/src/components/EditItineraryComp.vue
@@ -8,57 +8,33 @@
     <br />
 
     <form
-      id="Edit-itinerary"
+      id="itinerary"
       v-on:submit.prevent="updateItinerary()"
       v-if="showForm"
     >
-      <div class="input-name">
+
+      <div class="input-container">
         <label for="name">Itinerary Name:</label>
-        <input id="name" type="text" v-model="editedItinerary.itineraryName" />
+        <input id="name" type="text" class="input-field" v-model="editedItinerary.itineraryName" />
       </div>
-      <div class="input-starting-location">
+      <div class="input-container">
         <label for="name">Starting Location (Address):</label>
         <input
           id="starting-location"
-          type="text"
+          type="text" 
+          class="input-field"
           v-model="editedItinerary.startingLocation"
         />
       </div>
 
-      <div class="input-latitude">
-        <label for="latitudeInput">Latitude Coordinates:</label>
-        <input
-          type="number"
-          step="0.000001"
-          min="-90"
-          max="90"
-          name="latitudeInput"
-          id="latitudeInput"
-          v-model="editedItinerary.latitude"
-        />
-      </div>
-
-      <div class="input-longitude">
-        <label for="longitudeInput">Longitude Coordinates:</label>
-        <input
-          type="number"
-          step="0.000001"
-          min="-90"
-          max="90"
-          name="longitudeInput"
-          id="longitudeInput"
-          v-model="editedItinerary.longitude"
-        />
-      </div>
-
       <div class="create-buttons">
-        <input v-on:click.prevent="hideForm" type="button" value="Cancel" />
-        <input
+        <input class="cancel-button" v-on:click.prevent="hideForm" type="button" value="Cancel" />
+        <input class="reset-button"
           v-on:click.prevent="resetForm()"
           type="button"
           value="Reset Form"
         />
-        <button type="submit">Update</button>
+        <button class="submit-button" type="submit">Update</button>
       </div>
     </form>
   </div>
@@ -157,32 +133,116 @@ export default {
 </script>
 
 <style scoped>
+
 form {
-  text-align: center;
-  font-size: 40px;
-  border: 2px solid;
-  color: #519bcb;
-  font-family: Arial, "Times New Roman", Times, serif;
-  background-color: #ffffffdd;
+    text-align: center;
+    font-size: 30px;
+    font-weight: bold;
+    border: 2px solid;
+    color: #519BCB;
+    background-color: rgba(13, 13, 17, 0.667);
+    margin: 0 auto;
+    border-radius: 15px;
+    padding: 40px 0;
+    max-width: 900px;
+    display: flex; /* Use flexbox to align form elements */
+    flex-direction: column; /* Stack elements vertically */
+    align-items: center; /* Center items horizontally */
+}
+
+.input-name,
+.input-starting-location {
+  display: flex;
+  flex-direction: row; /* Change to column to stack inputs vertically */
+  align-items: center;
+  width: 100%;
+}
+
+label.input {
+  padding: 15px;
+    margin-bottom: 5px;
+}
+
+.input {
+  width: 300px; /* Use full width for input fields */
+  border-radius: 15px;
+ 
+}
+
+/* Add this style for input labels */
+.input-label {
+  width: 200px; /* Set a fixed width for labels to align them */
+  text-align: right;
+  padding-right: 10px;
+}
+
+/* Add this style for input fields */
+.input-field {
+  flex: 1; /* Expand input fields to fill remaining space */
+  padding: 8px;
+  border-radius: 15px;
+  border: 1px solid #ccc;
+}
+
+/* Add this style for input containers */
+.input-container {
+  display: flex;
+  align-items: center;
+  margin-bottom: 15px;
+}
+
+div.edit-form-button {
   margin: 0 auto;
-  border-radius: 5px;
 }
 
 .edit-header {
-    font-size: 50px;
+  font-size: 50px;
   text-align: center;
   border: 2px solid;
-  width: 350px;
+  width: 450px;
   height: 80px;
   margin: 0 auto;
   color: white;
   background-color: #519BCB;
   border-radius: 15px;
+
 }
 
-.edit-header:hover {
-    color: darksalmon;
+.cancel-button {
+  padding: 4px;
+  margin: 15px;
+  border-radius: 15px;
+  font-size: 80%;
 }
 
+.cancel-button:hover {
+  cursor: pointer;
+}
+
+.reset-button {
+  padding: 4px;
+  margin: 15px;
+  border-radius: 15px;
+  font-size: 80%;
+}
+
+.reset-button:hover {
+  cursor: pointer;
+}
+
+.submit-button {
+  padding: 4px;
+  margin: 15px;
+  border-radius: 15px;
+  font-size: 80%;
+}
+
+.submit-button:hover {
+  cursor: pointer;
+}
+
+.create-header:hover {
+  color: darksalmon;
+}
 
 </style>
--- vue/src/components/LandmarksComp.vue
+++ vue/src/components/LandmarksComp.vue
@@ -3,17 +3,11 @@
   <h1>Landmarks</h1>
     <div class="LandmarksGrid">
        <draggable class="draggable-landmarks" v-model="landmarks" group="cards" @start="drag=true" @end="drag=false" @change="addNewDestinationToItinerary(itineraryId, $event)">
-      <div
-        class="LandmarksList"
-        v-for="(landmark, index) in landmarks"
-        :key="index"
-       
-      >
-        <router-link
-          :to="{ name: 'landmarkdetail', params: { id: landmark.landmark_id } }"
-        >
-          <h2>{{ landmark.name }}</h2>
+      <div class="LandmarksList" v-for="(landmark, index) in landmarks" :key="index">
+        <router-link :to="{ name: 'landmarkdetail', params: { id: landmark.landmark_id } }" >
+          <h2 class="landmark-name">{{ landmark.name }}</h2>
         </router-link>
+        <br>
         <div class="landmark-image-container">
           <img class="landmark-image" v-bind:src="landmark.imageName" />
         </div>
@@ -113,18 +107,20 @@ export default {
 .LandmarksGrid {
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-  gap: 40px;
+  gap: 20px;
   text-decoration: none;
 }
 
 .LandmarksList {
   border: 2px solid #2ac1d7;
-  padding: 20px;
+  padding: 10px;
   font-family: 'Inter', sans-serif;
+  border-radius: 5px;
 }
 
-h2 {
+h2.landmark-name {
   font-size: 20px;
+  text-align: center;
 }
 
 div.landmark-image-container {
@@ -135,8 +131,8 @@ img.landmark-image {
   display: block;
   margin: 0 auto;
   padding-bottom: 20px;
-  max-width: 300px;
-  max-height: 300px;
+  max-width: 280px;
+  max-height: 280px;
 }
 
 .draggable-landmarks {
