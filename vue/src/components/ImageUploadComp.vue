d073d6012e039c5544e6e6185a8b216d133960af
Christopher Spencer <cdspenzer@gmail.com>
2023-08-16 12:43:47 -0400

    pulling

--- vue/src/components/ImageUploadComp.vue
+++ vue/src/components/ImageUploadComp.vue
@@ -9,6 +9,7 @@
       </button>
     </div>
   </template>
+
   <script>
   export default {
     name: "HelloWorld",
@@ -43,20 +44,7 @@
     },
   };
   </script>
-  <!-- Add "scoped" attribute to limit CSS to this component only -->
+
   <style scoped>
-  h3 {
-    margin: 40px 0 0;
-  }
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-  a {
-    color: #42B983;
-  }
+
   </style>
